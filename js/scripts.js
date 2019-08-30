154292b412d31d5212ba09454d9b1fa302c530a6
Molly Stambaugh <molly.stambaugh@gmail.com>
2019-08-30 13:40:47 -0700

    add outline of objects and prototypes

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,42 @@
 // Business End
-function Order() {
-  this.OrderItem = []
+function Cart() {
+  this.OrderItem = [],
+  this.OrderItemPrice = [],
 
 }
 
-Order.prototype.addOrderItem = function(order)
-  this.OrderItem.push(order)
+Cart.prototype.addOrderItem = function(item) {
+  this.OrderItem.push(item)
+}
+
 
 function Item() {
   this.customer = "name"
   this.expensivetopping = [],
-  this.cheaptopping = []
-  // this.sauce = ()
-  // this.crust = gluten-free, thin, thick
+  this.cheaptopping = [],
+  this.sauce = (""),
+  this.crust = (),
 }
 
+Item.prototype.removeOrderItem = function(remove)
+  this.Item
 
+Item.prototype.addToCart = function() {
+  var ordertotal = 0;
+  this.expensivetopping.forEach(function(val) {
+    order = expensivetopping + val;
+  });
+  this.totalscore += turnpoints;
+  player1.turn = true
+  player2.turn = true
+  this.turn = false
+  this.turnscore = [];
+  if (this.totalscore >= 100) {
+    game.endGame();
+  }
+}
 // Front End
-$(document).ready(function(){
+$(document).ready(function() {
   $("#order").submit(function(event) {
     event.preventDefault();
     var customer = $("#customer-name").val();
