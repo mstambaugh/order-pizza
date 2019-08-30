5711398b8c7da0da5f4480d9d8e890c2d6b4d181
Molly Stambaugh <molly.stambaugh@gmail.com>
2019-08-30 16:15:54 -0700

    add js

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,9 @@
 // Business End
 function Cart() {
-  this.OrderItem = [],
-  this.OrderItemPrice = [],
-
+  this.OrderItems = [],
+  this.OrderPrices = [],
+  this.OrderTotal = 0,
+  this.customer = "name"
 }
 
 Cart.prototype.addOrderItem = function(item) {
@@ -10,29 +11,28 @@ Cart.prototype.addOrderItem = function(item) {
 }
 
 
-function Item() {
-  this.customer = "name"
-  this.expensivetopping = [],
-  this.cheaptopping = [],
-  this.sauce = (""),
-  this.crust = (),
-}
+// function Item(expensivetopping, cheaptopping, sauce, crust) {
+//   this.expensivetopping = expensivetopping,
+//   this.cheaptopping = cheaptopping,
+//   this.sauce = sauce,
+//   this.crust = crust
+// }
 
 Item.prototype.removeOrderItem = function(remove)
   this.Item
 
-Item.prototype.addToCart = function() {
+Item.prototype.addOrderItem = function() {
   var ordertotal = 0;
   this.expensivetopping.forEach(function(val) {
     order = expensivetopping + val;
-  });
-  this.totalscore += turnpoints;
-  player1.turn = true
-  player2.turn = true
-  this.turn = false
-  this.turnscore = [];
-  if (this.totalscore >= 100) {
-    game.endGame();
+  // });
+  // this.totalscore += turnpoints;
+  // player1.turn = true
+  // player2.turn = true
+  // this.turn = false
+  // this.turnscore = [];
+  // if (this.totalscore >= 100) {
+  //   game.endGame();
   }
 }
 // Front End
