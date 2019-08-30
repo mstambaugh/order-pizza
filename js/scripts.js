37778b93375e3b8302e1567fc9bc4bd05fc4233e
Molly Stambaugh <molly.stambaugh@gmail.com>
2019-08-30 11:00:06 -0700

    add html and initial js

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+// Business End
+function Order ()
+
+Order.protoype.addOrder
+
+
+// Front End
+$(document).ready(function(){
+  $("#start").submit(function(event) {
+    event.preventDefault();
+});
