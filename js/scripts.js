1fa8cd6953293cfebd7d2588a295698e8f018e9a
Molly Stambaugh <molly.stambaugh@gmail.com>
2019-08-30 12:25:59 -0700

    add html- checkboxes

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,18 @@
 // Business End
 function Order() {
   this.OrderItem = []
-  this.customer = "name"
+
 }
 
 Order.prototype.addOrderItem = function(order)
   this.OrderItem.push(order)
 
 function Item() {
+  this.customer = "name"
   this.expensivetopping = [],
   this.cheaptopping = []
-  this.sauce = ()
+  // this.sauce = ()
+  // this.crust = gluten-free, thin, thick
 }
 
 
@@ -18,5 +20,11 @@ function Item() {
 $(document).ready(function(){
   $("#order").submit(function(event) {
     event.preventDefault();
+    var customer = $("#customer-name").val();
+
+
   });
 });
+
+// check boxes for 2 topping types, drop boxes for sauce and crust
+// username
